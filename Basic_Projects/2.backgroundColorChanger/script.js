3d018c56c895ec1faba010af5422ba59b9a79f5a
Amritanshu Shukla <amritanshu.shukla09@gmail.com>
2026-01-12 11:51:20 +0530

    Delete Basic_Projects directory

--- Basic_Projects/2.backgroundColorChanger/script.js
+++
@@ -1,6 +0,0 @@
-const parent = document.getElementById("parent");
-parent.addEventListener("click", (e) => {
-  const btn = e.target;
-  const body = document.querySelector("body");
-  body.style.backgroundColor = btn.id;
-});
